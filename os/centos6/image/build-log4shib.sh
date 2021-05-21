9567d887394725b885ecce92b154446f7df9b61a
John W. O'Brien <john@saltant.com>
2021-05-21 14:39:20 +0000

    Drop support for CentOS 6

--- os/centos6/image/build-log4shib.sh
+++
@@ -1,3 +0,0 @@
-rpmbuild -ba --clean log4shib.spec
-rpmspec -q --rpms log4shib.spec > ${EXT_BASE}/out/log4shib.rpms
-rpmspec -q --srpm log4shib.spec > ${EXT_BASE}/out/log4shib.srpm
--- os/centos6/image/build-xerces-c.sh
+++
@@ -1,3 +0,0 @@
-rpmbuild -ba --clean xerces-c.spec
-rpmspec -q --rpms xerces-c.spec > ${EXT_BASE}/out/xerces-c.rpms
-rpmspec -q --srpm xerces-c.spec > ${EXT_BASE}/out/xerces-c.srpm
